6a92a4efe3a07dcaa0b45edc9d6e2d44dc2b0a5a
Twixyy8 <145301714+Twixyy8@users.noreply.github.com>
2024-05-05 14:37:08 +0100

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -1,21 +1,3 @@
-const { EmbedBuilder } = require('discord.js');
-
-module.exports = {
-    name: 'owner',
-    description: 'Bot owner info',
-    execute(message, args) {
-        const youtubeLink = 'https://discord.gg/FUEHs7RCqz';
-        const InstagramLink = 'https://discord.gg/FUEHs7RCqz';
-        const embed = new EmbedBuilder()
-            .setColor('#FFFFFF')
-            .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖 Myself Shiva aka RTX. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [RTX GAMING](${youtubeLink})\n 💙 [Atstreak](${InstagramLink})`)
-            .setTimestamp();
-
-
-        message.reply({ embeds: [embed] });
-    },
-};
 
 /*
 
